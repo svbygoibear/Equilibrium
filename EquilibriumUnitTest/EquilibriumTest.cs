bfdc625e024b94d3aa6f1bb3649e45d3f3f74850
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 22:18:27 +0200

    Added a basic unit test.

---
+++ EquilibriumUnitTest/EquilibriumTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EquilibriumUnitTest {
+    [TestClass]
+    public class EquilibriumTest {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
