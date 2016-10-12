88228f08655673242c2e5b4181cdde21925df7f0
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 22:21:36 +0200

    Finished up all unit tests - passed all  of them. Just to fix readme file.

--- EquilibriumUnitTest/EquilibriumTest.cs
+++ EquilibriumUnitTest/EquilibriumTest.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Equilibrium;
 
 namespace EquilibriumUnitTest {
     [TestClass]
     public class EquilibriumTest {
         [TestMethod]
-        public void TestMethod1() {
+        public void CheckEquilibrium1() {
+            var input = new List<int> { 1, 2, 3, 4, 5, 7, 8, 10, 12 };
+            var result = input.getEquilibrium();
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void CheckEquilibrium2() {
+            var input = new List<int> { 1, 4, 5, 6, 8, 10, 17 };
+            var result = input.getEquilibrium();
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void CheckEquilibrium3() {
+            var input = new List<int> { 12, 56, 5, 6, 7, 9 };
+            var result = input.getEquilibrium();
+
+            Assert.AreEqual(-1, result);
         }
     }
 }
