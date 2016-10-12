f92baa8a2a5883f06d0dfaff0bd7f54e48225bff
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 22:09:26 +0200

    Finished up most of the equilibrium class, added a singular equilibrium point as well as an internal recursive method for calculating equilibriu.

--- Equilibrium/Equilibrium.cs
+++ Equilibrium/Equilibrium.cs
@@ -5,6 +5,24 @@ using System.Text;
 
 namespace Equilibrium {
     public static class Equilibrium {
+        public static int getEquilibrium(this List<int> numbers) {
+            if (numbers.Count == 0)
+                return -1;
+
+            var sum = getSum(numbers);
+            return calculateEquilibrium(numbers, sum, 0, sum, 0);
+        }
+
+        private static int calculateEquilibrium(List<int> numbers, int leftAmount, int rightAmount, int sum, int curr) {
+            if (rightAmount == leftAmount)
+                return curr - 1;
+            else if (rightAmount > leftAmount)
+                return -1;
+            else
+                return calculateEquilibrium(numbers, leftAmount - numbers[curr],
+                    curr > 0 ? getSumFromRight(numbers, curr) : 0, sum, curr + 1);
+        }
+
         private static int getSumFromRight(List<int> numbers, int countFromRight) {
             var total = 0;
             for (int i = 0; i < countFromRight; i++)
