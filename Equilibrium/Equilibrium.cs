5a642948db942f527f1885c2e70e9062336e1d02
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 21:52:36 +0200

    Added a get sum from right method to caclulate the sum depending on the index to determine the right side total.

--- Equilibrium/Equilibrium.cs
+++ Equilibrium/Equilibrium.cs
@@ -5,6 +5,14 @@ using System.Text;
 
 namespace Equilibrium {
     public static class Equilibrium {
+        private static int getSumFromRight(List<int> numbers, int countFromRight) {
+            var total = 0;
+            for (int i = 0; i < countFromRight; i++)
+                total += numbers[i];
+
+            return total;
+        }
+
         private static int getSum(List<int> numbers) {
             var result = 0;
             numbers.ForEach(number => {
